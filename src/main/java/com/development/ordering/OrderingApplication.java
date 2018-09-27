bf5dd78d9b0e2fb19de74901a1e15e6e522fdb66
twinster <igochitashvili11@gmail.com>
2018-09-27 12:22:48 +0200

    Initializing Project

---
+++ src/main/java/com/development/ordering/OrderingApplication.java
@@ -0,0 +1,12 @@
+package com.development.ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderingApplication.class, args);
+    }
+}
